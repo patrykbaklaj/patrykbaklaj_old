e491a9d50107a2b2fceee8bac6793411fa19dd6a
Patryk Bakłaj <Patryk Bakaj>
2017-10-26 20:18:09 +0100

    Add About section

--- build/out.js
+++ build/out.js
@@ -77,17 +77,50 @@ module.exports = __webpack_require__(2);
 
 $(document).ready(function() {
 
+  setWindowHeight();
 
-  $("body").fadeIn('slow', animateH1);
+  $("body").fadeIn('fast', animateH1);
+
+
+  $(window).on("load resize", function () {
+    setWindowHeight();
+  });
+
   $(".arrow img").click(function() {
     scrollDown();
   });
 
 
+  // $('body, html').scrollspy({
+  //   target: '#navbar',
+  //   offset: 150
+  // })
+  //
+  // $('nav a, #to-top').bind('click', function() {
+  //   $('html, body').stop().animate({
+  //     scrollTop: $($(this).attr('href')).offset().top - 50
+  //   }, 1400, 'easeInExpo');
+  //   event.preventDefault();
+  // })
+  //
+  // $('.carousel a.btn-primary').bind('click', function() {
+  //   $('html, body').stop().animate({
+  //     scrollTop: $('#services').offset().top - 50
+  //   }, 1200, 'easeInExpo');
+  //   event.preventDefault();
+  // })
+
+
+  function setWindowHeight() {
+    $(".fill-screen").css("height", window.innerHeight);
+  }
+
+  //
+  //
+
   // first section animation
   function animateH1 (){
-    $(".welcome h1").delay(500)
-    .css("margin-top", "20%")
+    $(".hello-text").delay(600)
     .fadeIn("slow")
     .delay(1500)
     .fadeOut("400", function(){
@@ -96,34 +129,25 @@ $(document).ready(function() {
     .fadeIn()
     .delay(1500)
     .fadeOut("400", function(){
-      $('.welcome').addClass('background_welcome parallax');
+      $('.welcome').addClass('background-welcome parallax');
       // $(this).html("<a href='#'>Let's get started!</a>");
-      $(this).html("<span>Let's get started!</span>");
-      $(this).css({
-        "background": "black",
-        "opacity": "0.7",
-        "width": "100%"
-      })
+      $(this).text("Let's get started!");
+      $(this).addClass("hello-text-after")
     })
-    .fadeIn("fast")
-    .animate({
-      fontSize: "4em",
-      fontWeight: "bold",
-      padding: "1em"
-    }, 100, showArrow)
+    .fadeIn("400", showArrow);
   };
 
   function showArrow(){
     $(".arrow").fadeIn();
   }
-
+  //
   function scrollDown(){
     $('html, body').animate({
-        scrollTop: $(".about").offset().top
+      scrollTop: $("#about").offset().top
     }, 800);
 
   }
-
+  //
 });
 
 
